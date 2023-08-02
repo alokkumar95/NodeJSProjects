0a397a8bb4f380534ca627bb5b9d0fc86f41eba2
alok <alok@aloks-MacBook-Pro.local>
2023-08-02 18:06:45 +0530

    updated cros-fit-app

--- crossfit-wod-api/src/database/Workout.js
+++ crossfit-wod-api/src/database/Workout.js
@@ -1,8 +1,21 @@
 // In src/database/Workout.js
 const DB = require("./db.json");
+const saveToDatabase = require("./utils");
 
 const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-module.exports = { getAllWorkouts };
+const createNewWorkOut = (newWorkOut) => {
+  const isAlreadyAdded =
+    DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+  if (isAlreadyAdded) {
+    return;
+  }
+
+  DB.workouts.push(newWorkOut);
+  saveToDatabase(DB);
+  return newWorkOut;
+};
+
+module.exports = { getAllWorkouts, createNewWorkOut };
